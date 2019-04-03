2a2d714872d56dbc5cd931c17ee76ca29a977c8e
liujianlin <liujianlin@imsightmed.com>
2019-04-03 18:48:35 +0800

    use float disparity

--- dataloader/KITTIObjectLoader.py
+++ dataloader/KITTIObjectLoader.py
@@ -6,6 +6,7 @@ import torch
 import torchvision.transforms as transforms
 import random
 from PIL import Image, ImageOps
+import cv2
 import numpy as np
 import preprocess
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -54,29 +55,36 @@ class KITTIObjectLoader(data.Dataset):
                 if u < 0 or u >= img_width or v < 0 or v >= img_height:
                     continue
                 disp_L[v][u] = calib.P[0][0] * self.baseline / pc_rect[i][2]
-            # fname = os.path.join(self.disp_dir, '{:06}.npy'.format(data_idx))
-            # np.save(fname, disp_L)
-            fname = os.path.join(self.disp_dir, '{:06}.png'.format(data_idx))
-            cv2.imwrite(fname, disp_L.astype(np.int32))
+            fname = os.path.join(self.disp_dir, '{:06}.npy'.format(data_idx))
+            np.save(fname, disp_L)
+            # fname = os.path.join(self.disp_dir, '{:06}.png'.format(data_idx))
+            # cv2.imwrite(fname, disp_L.astype(np.int32))
 
     def __getitem__(self, index):
         left = os.path.join(self.left_dir, '{0}.png'.format(self.frame_ids[index]))
         right = os.path.join(self.right_dir, '{0}.png'.format(self.frame_ids[index]))
-        disp_L = os.path.join(self.disp_dir, '{0}.png'.format(self.frame_ids[index]))
+        # disp_L = os.path.join(self.disp_dir, '{0}.png'.format(self.frame_ids[index]))
+        disp_L = os.path.join(self.disp_dir, '{0}.npy'.format(self.frame_ids[index]))
         left_img = Image.open(left).convert('RGB')
         right_img = Image.open(right).convert('RGB')
-        dataL = Image.open(disp_L)
+        # dataL = Image.open(disp_L)
+        dataL = np.load(disp_L)
 
+        target_w = 1248
+        target_h = 352
         w, h = left_img.size
 
-        left_img = left_img.crop((w-1248, h-352, w, h))
-        right_img = right_img.crop((w-1248, h-352, w, h))
+        # this will add zero padding
+        left_img = left_img.crop((w-target_w, h-target_h, w, h))
+        right_img = right_img.crop((w-target_w, h-target_h, w, h))
         w1, h1 = left_img.size
 
-        # TODO: save and load sparse disparity as float, need to implement crop on numpy
-        dataL = dataL.crop((w-1248, h-352, w, h))
-        dataL = np.ascontiguousarray(dataL,dtype=np.float32)
-        #dataL = np.zeros_like(dataL, dtype=np.float32)
+        # load sparse disparity as float, need to implement crop on numpy
+        # print('origin size: ', dataL.shape)
+        dataL = np.pad(dataL, ((max(target_h-h, 0), 0), (max(target_w-w, 0), 0)), 'constant', constant_values=0)
+        # print('after padding: ', dataL.shape)
+        dataL = dataL[max(dataL.shape[0]-target_h, 0):dataL.shape[0], max(dataL.shape[1]-target_w, 0):dataL.shape[1]]
+        # print('after crop: ', dataL.shape)
 
         processed = preprocess.get_transform(augment=False)
         left_img = processed(left_img)
@@ -90,7 +98,7 @@ class KITTIObjectLoader(data.Dataset):
 
 if __name__ == '__main__':
     loader = KITTIObjectLoader(sys.argv[1], sys.argv[2])
-    #loader.generate_sparse_disparity()
-    print(loader[0])
-    disp = loader[0][2]
-    print(torch.FloatTensor(disp).sign())
+    loader.generate_sparse_disparity()
+    # print(loader[0])
+    # disp = loader[0][2]
+    # print(torch.FloatTensor(disp).sign())
